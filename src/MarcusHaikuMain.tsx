f6a570381e24d3b4b3af6e83ba6a364ce06a6bab
AKB <akb@AKBs-MacBook-Air.local>
2024-02-28 17:00:21 -0800

    tailwind and new background

--- src/MarcusHaikuMain.tsx
+++ src/MarcusHaikuMain.tsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
-import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import { useState, useEffect } from "react";
 import { lineGenerator } from "./data/utils";
-import { Button } from "@mui/material";
-import bg from './bg-roman.png'
 import useIsMobile from "./hooks/useMobile";
+import './App.css'; 
 
 const emptyHaiku = {
   lineOne: "",
@@ -14,7 +12,8 @@ const emptyHaiku = {
 const MarcusHaikuMain = () => {
   const [haiku, setHaiku] = useState(emptyHaiku);
   useEffect(() => haikuGenerator(), []);
-const isMobile = useIsMobile();
+  const isMobile = useIsMobile();
+
   const haikuGenerator = () => {
     let currentHaiku = emptyHaiku;
     currentHaiku.lineOne = lineGenerator(5);
@@ -24,47 +23,24 @@ const isMobile = useIsMobile();
   };
 
   return (
-    <Grid container sx={{backgroundImage: `url(${bg})`, 
-    backgroundSize: '60%',
-    backgroundRepeat: 'no-repeat', 
-    backgroundAttachment: 'fixed', 
-    backgroundPosition: 'center center', 
-    backgroundColor:"#D2C5BB",
-    height:'120vh'}}>
-      <Grid m={3}>
-        <Grid flexDirection="column"  >
-          <h1>Random Marcus Aurelius haiku generator</h1>
-          <Grid sx={isMobile? { margin: 4 }:{ margin: 4 }}/>
-          <p>
-            {haiku.lineOne}
-            <br />
-            {haiku.lineTwo}
-            <br />
-            {haiku.lineThree}
-          </p>
-          <Grid sx={isMobile? { margin: 4 }:{ margin: 4 }}/>
-          <Button variant="outlined" onClick={haikuGenerator} sx={{textTransform:'none'}}>
-            Generate New Haiku
-          </Button>
-        </Grid>
-        <Grid sx={isMobile? { marginTop: 14 }:{ marginTop: 4 }}>
-          <h3>How are the haikus generated?</h3>
-          <p>
-            {`Every word from Marcus Aurelius' first three books of Meditations was
-          put through an algorithm to determine how many syllables the word
-          contained. The lines were randomly created using the traditional haiku
-          pattern of 5 syllables for the first line, 7 syllables for
-          the second line and 5 syllables for the third and final line. `}
-          </p>
-          <h3>Some thoughts...</h3>
-          <p>
-            {`This is a great use case for Augmented
-          Intelligence, in other words, using the computer to augment the creativity
-          of the human mind. Specifically, the human mind can more easily determine the number of syllables in a word, and the machine may be able to "randomly" generate combinations of words that would not immediately come to the human mind. Sometimes the computer gets the number of variables wrong, or the sentence doesn't quite make sense, but that's where you come in!`}
-          </p>
-        </Grid>
-      </Grid>
-    </Grid>
+    <div className="background-pan">
+    <div className="text-center text-white z-10">
+      <h1 className="spacing-p-b">Meditations Haiku Generator</h1>
+      <div className="spacing-p-b">
+        <h2 className="">
+          {haiku.lineOne}<br />
+          {haiku.lineTwo}<br />
+          {haiku.lineThree}
+        </h2>
+      </div>
+      <button 
+        onClick={haikuGenerator} 
+        className="button">
+        Generate New Haiku
+      </button>
+    </div>
+  </div>
+  
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import MarcusHaikuMain from "./MarcusHaikuMain";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./theme";
+import './App.css'; 
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,13 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <MarcusHaikuMain />
-    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
